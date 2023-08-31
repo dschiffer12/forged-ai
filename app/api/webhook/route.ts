beae70fd275a4635ad696636ebc93fc0c5661c03
dschiffer12 <dschiffer12@gmail.com>
2023-08-31 17:06:11 -0600

    Route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,7 +1,6 @@
 import Stripe from "stripe"
 import { headers } from "next/headers"
 import { NextResponse } from "next/server"
-
 import prismadb from "@/lib/prismadb"
 import { stripe } from "@/lib/stripe"
 
